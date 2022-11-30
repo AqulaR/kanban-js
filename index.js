89da6e1f32d229bc7ef0008eb200e6815e26d6d4
AqulaR <103633654+AqulaR@users.noreply.github.com>
2022-11-30 11:25:35 +0300

    11-30-2022

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const cors = require("cors");
-const morgan = require("morgan");
-//const { faker } = require('@faker-js/faker/locale/ru');
 const app = express();
 const server = http.createServer(app);
 const fs = require('fs');
 
-let data = require('./views/data.json');
+let jsonfile = require('jsonfile');
+let file = jsonfile.readFileSync('data.json');
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
 app.disable("x-powered-by");
 
 app.use((err, req, res, next) => {
@@ -30,44 +26,46 @@ app.use((err, req, res, next) => {
 	}
 });
 
-app.set('view engine', 'pug')
 
-app.use(express.static("views"));
-
-app.use('/dashboard', function (request, res) {
-	res.render('dashboard', {
-		data:data,
-	})
-})
-
-let jsonfile = require('jsonfile');
-
-let file = jsonfile.readFileSync('data.json')
-
-app.put('/edit/:id', function(req, res) {
-    let id = req.params.id;
-    let newText = req.body.text;
-
-    jsonfile.readFile('data.json', function(err, obj) {
-      let fileObj = obj;
-      fileObj[id].text = newText;
-      jsonfile.writeFile('data.json', fileObj, function(err) {
-          if (err) throw err;
-      });
-      res.send(obj)
-    });
+app.get('/book', (req, res) => {
+  res.status(200).type('text/plain')
+  res.send(JSON.stringify(file, null, '\t'))
 });
 
-app.post('/addTask', (req, res) => {
+
+app.get('/book/:id', (req, res) => {
+  res.status(200).type('text/plain')
+  let id = req.params.id;
+  res.send(JSON.stringify(file[id], null, '\t'))
+});
+
+app.get('/book/:id/users', (req, res) => {
+  res.status(200).type('text/plain')
+  let id = req.params.id;
+  res.send(JSON.stringify(file[id].users, null, '\t'))
+});
+
+app.get('/book/:bid/:uid', (req, res) => {
+  res.status(200).type('text/plain')
+  let bid = req.params.bid;
+  let uid = req.params.uid;
+  res.send(JSON.stringify(file[bid].users[uid], null, '\t'))
+});
+
+app.post('/book', (req, res) => {
   if (!req.body) return res.sendStatus(400)
-  const user = {
+  const book = {
     id: file.length,
-    text: req.body.text,
+    amount: req.body.amount,
+    name: req.body.name,
+    author: req.body.author,
+    relise: req.body.relise,
+    users: []
   }
   jsonfile.readFile('data.json', (err, obj) => {
     if (err) throw err
     let fileObj = obj;
-    fileObj.push(user);
+    fileObj.push(book);
     jsonfile.writeFile('data.json', fileObj, (err) => {
       if (err) throw err;
     })
@@ -75,7 +73,67 @@ app.post('/addTask', (req, res) => {
   })
 })
 
-app.delete('/delete/:id', (req, res) => {
+app.post('/book/:id/users', (req, res) => {
+  res.status(200).type('text/plain')
+  let id = req.params.id;
+  if (!req.body) return res.sendStatus(400)
+  const users = {
+      id: file[id].users.length,
+      name: req.body.name,
+      datein: req.body.datein,
+      dateout: ""
+  }
+  jsonfile.readFile('data.json', (err, obj) => {
+    if (err) throw err
+    let fileObj = obj[id].users;
+    fileObj.push(users);
+    jsonfile.writeFile('data.json', obj, (err) => {
+      if (err) throw err;
+    })
+    res.send(obj)
+  })
+});
+
+app.put('/book/:id', function(req, res) {
+  let id = req.params.id;
+  let amount = req.body.amount;
+  let name = req.body.name;
+  let author = req.body.author;
+  let relise = req.body.relise;
+
+  jsonfile.readFile('data.json', function(err, obj) {
+    let fileObj = obj;
+    fileObj[id].amount = amount;
+    fileObj[id].name = name;
+    fileObj[id].author = author;
+    fileObj[id].relise = relise;
+    jsonfile.writeFile('data.json', fileObj, function(err) {
+        if (err) throw err;
+    });
+    res.send(obj)
+  });
+});
+
+app.put('/book/:bid/:uid', function(req, res) {
+  let bid = req.params.bid;
+  let uid = req.params.uid;
+  let name = req.body.name;
+  let datein = req.body.datein;
+  let dateout = req.body.dateout;
+
+  jsonfile.readFile('data.json', function(err, obj) {
+    let fileObj = obj;
+    fileObj[bid].users[uid].name = name;
+    fileObj[bid].users[uid].datein = datein;
+    fileObj[bid].users[uid].dateout = dateout;
+    jsonfile.writeFile('data.json', fileObj, function(err) {
+        if (err) throw err;
+    });
+    res.send(obj)
+  });
+});
+
+app.delete('/book/:id', (req, res) => {
   jsonfile.readFile('data.json', (err, obj) => {
     if (err) throw err
     let fileObj = obj;
